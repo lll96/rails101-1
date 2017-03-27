322fdf944989adfdb8967720c5e0a5ef609927e4
hy <15871480943@163.com>
2017-03-27 12:51:21 +0800

    add post button

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -7,12 +7,13 @@ class GroupsController < ApplicationController
   end
 
   def show
-     @group = Group.find(params[:id])
-     @posts = @group.posts
-   end
+   @group = Group.find(params[:id])
+   @posts = @group.posts
+ end
+
 
   def edit
-    
+
     if current_user != @group.user
       redirect_to root_path, alert: "You have no permission."
     end
