b2885c43ae7745ab64de20763254a8279ae2e1fa
hy <15871480943@163.com>
2017-03-28 08:48:32 +0800

    implement post create function

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,8 @@
 class Post < ApplicationRecord
   belongs_to :user
-  belongs-to :group 
+  belongs_to :group
+
+  validates :content, presence: true
+  
   scope :recent, -> { order("created_at DESC")}
 end
